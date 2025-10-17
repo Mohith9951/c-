6c216c773e4acd23415f417da9739c388ec04000
Mohith9951 <24b11ds011@adityauniversity.in>
2025-10-17 11:36:41 +0530

    Add files via upload

---
+++ whileloop 2.cpp
@@ -0,0 +1,16 @@
+//wap to print sum of 1 to n
+#include<stdio.h>
+int main()
+{
+	int i,n,sum=0;
+	printf("enter n value:");
+	scanf("%d",&n);
+	i=1;
+	while(i<=n)
+	{
+		sum=sum+i;
+		i++;
+	}
+	printf("\t%d",sum);
+	return 0;
+}
